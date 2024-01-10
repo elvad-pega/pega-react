f2a3d28c2225d0fee105cb96692ab2c8fa138daf
elvad-pega <elvad@pega.com>
2024-01-10 20:20:45 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store } from "./_helpers";
+import AppSelector from "./_apps/AppSelector/AppSelector";
+
+import "./_styles/index.css";
+import "semantic-ui-css/semantic.min.css";
+import "./_styles/semantic-custom.css"
+import "react-datepicker/dist/react-datepicker.css";
+import "./_styles/uikit/pega-icons.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppSelector />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
